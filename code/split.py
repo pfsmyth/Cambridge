56625b62018942b5b57c1515039b6fbd19e88c68
pfsmyth <psmyth@bcs.org.uk>
2016-12-12 14:47:32 +0000

    Latest versions

--- code/split.py
+++ code/split.py
@@ -3,8 +3,13 @@
 mystring = "5,GER,17,10,15,42,View medals by sport for Germany"
 
 mylistofitems = mystring.split(",")
-for item in mylistofitems :
-    print item
+
+print(mylistofitems)
+
+print(mylistofitems[1])
 
 for item in mylistofitems :
-    print type(item )   
+    print(item)
+
+for item in mylistofitems :
+    print(type(item))
